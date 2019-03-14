744d18b8d7a4cfecb71a06011f9064b90cb22765
Saket2012 <saketgonte@Sakets-Air.lan1>
2019-03-13 21:57:25 -0700

    Updated responses and added comments

--- articles.py
+++ articles.py
@@ -6,7 +6,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
+# HTTP Authentication
 def authorization():
     auth = request.authorization
     user_details = users_db.get_user_details(auth.username)
@@ -19,137 +19,185 @@ def authorization():
         else:
             return False
     else:
-        return False
+        return None
 
 
 class Articles(Resource):
 
+    # Post new article
     def post(self):
         data = request.get_json()
         text = data['text']
         author = data['author']
         title = data['title']
+        # Check NULL condition of all fields
         if text == "" or author == "" or title == "":
             response = Response(status = 400, mimetype = 'application/json')
             return response
         auth = authorization()
-        if auth:
-            authentication = request.authorization
-            user_id = users_db.get_user_details(authentication.username)
-            user_id = user_id[0]
-            articles_db.post_article(user_id[0], text, author, title)
-            article_id = articles_db.get_article(title)
-            article_id = article_id[0]
-            response = Response(status = 201, mimetype = 'application/json')
-            response.headers['location'] = 'http://127.0.0.1:5000/articles/' + str(article_id[0])
-            return response
+        # Check authentication
+        if auth is not None:
+            if auth:
+                authentication = request.authorization
+                user_id = users_db.get_user_details(authentication.username)
+                user_id = user_id[0]
+                # Post an article
+                articles_db.post_article(user_id[0], text, author, title)
+                # Get article id
+                article_id = articles_db.get_article(title)
+                article_id = article_id[0]
+                response = Response(status = 201, mimetype = 'application/json')
+                response.headers['location'] = 'http://127.0.0.1:5000/articles/' + str(article_id[0])
+                return response
+            else:
+                # Unauthorized access
+                response = Response(status = 401, mimetype = 'application/json')
+                return response
         else:
-            response = Response(status = 401, mimetype = 'application/json')
+            # User not found
+            response = Response(status = 404, mimetype = 'application/json')
             return response
 
+    # Update an article
     def put(self):
         data = request.get_json()
         title = data['title']
         author = data['author']
         text = data['text']
+        # Check NULL condition of all fields
         if text == "" or author == "" or title == "" or text == "":
             response = Response(status = 400, mimetype = 'application/json')
             return response
         auth = authorization()
-        if auth:
-            authentication = request.authorization
-            user_details = users_db.get_user_details(authentication.username)
-            user_details = user_details[0]
-            article_id = articles_db.get_article_details(user_details[0], title)
-            if not article_id:
-                response = Response(status = 404, mimetype = 'application/json')
-                return response
-            article_id = article_id[0]
-            article_user_id = article_id[1]
-            if article_user_id == user_details[0]:
-                articles_db.edit_article(title, author, text, article_id[0])
-                response = Response(status = 200, mimetype = 'application/json')
-                return response
+        # Check authentication
+        if auth is not None:
+            if auth:
+                authentication = request.authorization
+                user_details = users_db.get_user_details(authentication.username)
+                user_details = user_details[0]
+                article_id = articles_db.get_article_details(user_details[0], title)
+                if not article_id:
+                    # Check if article exists or not
+                    response = Response(status = 404, mimetype = 'application/json')
+                    return response
+                article_id = article_id[0]
+                article_user_id = article_id[1]
+                # Allow editing of an article posted by the same user
+                if article_user_id == user_details[0]:
+                    # Edit article
+                    articles_db.edit_article(title, author, text, article_id[0])
+                    response = Response(status = 200, mimetype = 'application/json')
+                    return response
+                else:
+                    # Unauthorized access
+                    response = Response(status = 401, mimetype = 'application/json')
+                    return response
             else:
+                # Unauthorized access
                 response = Response(status = 401, mimetype = 'application/json')
                 return response
         else:
-            response = Response(status = 401, mimetype = 'application/json')
+            # User not found
+            response = Response(status = 404, mimetype = 'application/json')
             return response
 
+    # Delete an article
     def delete(self):
         data = request.get_json()
         title = data['title']
+        # Check NULL condition of all fields
         if title == "":
             response = Response(status = 400, mimetype = 'application/json')
             return response
         auth = authorization()
-        if auth:
-            authentication = request.authorization
-            user_details = users_db.get_user_details(authentication.username)
-            user_details = user_details[0]
-            article_id = articles_db.get_article_details(user_details[0], title)
-            if not article_id:
-                response = Response(status = 404, mimetype = 'application/json')
-                return response
-            article_id = article_id[0]
-            article_user_id = article_id[1]
-            if article_user_id == user_details[0]:
-                articles_db.delete_article(article_id[0])
-                response = Response(status = 200, mimetype = 'application/json')
-                return response
+        # Check Authentication
+        if auth is not None:
+            if auth:
+                authentication = request.authorization
+                user_details = users_db.get_user_details(authentication.username)
+                user_details = user_details[0]
+                article_id = articles_db.get_article_details(user_details[0], title)
+                if not article_id:
+                    # Article not found
+                    response = Response(status = 404, mimetype = 'application/json')
+                    return response
+                article_id = article_id[0]
+                article_user_id = article_id[1]
+                if article_user_id == user_details[0]:
+                    # Delete an article
+                    articles_db.delete_article(article_id[0])
+                    response = Response(status = 200, mimetype = 'application/json')
+                    return response
+                else:
+                    # Unauthorized access
+                    response = Response(status = 401, mimetype = 'application/json')
+                    return response
             else:
+                # Unauthorized access
                 response = Response(status = 401, mimetype = 'application/json')
                 return response
         else:
-            response = Response(status = 401, mimetype = 'application/json')
+            # User not found
+            response = Response(status = 404, mimetype = 'application/json')
             return response
 
     def get(self):
         data = request.get_json()
         title = data['title']
+        # Check NULL condition of all fields
         if title == "":
             response = Response(status = 400, mimetype = 'application/json')
             return response
         auth = authorization()
-        if auth:
-            article = articles_db.get_article(title)
-            if not article:
-                response = Response(status = 404, mimetype = 'application/json')
+        if auth is not None:
+            if auth:
+                article = articles_db.get_article(title)
+                if not article:
+                    # Article not found
+                    response = Response(status = 404, mimetype = 'application/json')
+                    return response
+                article = article[0]
+                title = article[4]
+                author = article[3]
+                text = article[2]
+                response = {"Title": title,
+                            "Author": author,
+                            "Text": text}
                 return response
-            article = article[0]
-
-            title = article[4]
-            author = article[3]
-            text = article[2]
-            response = {"Title": title,
-                        "Author": author,
-                        "Text": text}
-            return response
+            else:
+                # Unauthorized access
+                response = Response(status = 401, mimetype = 'application/json')
+                return response
         else:
-            response = Response(status = 401, mimetype = 'application/json')
+            # User not found
+            response = Response(status = 404, mimetype = 'application/json')
             return response
-
 
 class N_Articles(Resource):
     def get(self, no_of_articles):
         auth = authorization()
-        if auth:
-            n_articles = articles_db.get_n_articles(no_of_articles)
-            if not n_articles:
-                response = Response(status = 404, mimetype = 'application/json')
+        if auth is not None:
+            if auth:
+                n_articles = articles_db.get_n_articles(no_of_articles)
+                if not n_articles:
+                    response = Response(status = 404, mimetype = 'application/json')
+                    return response
+                with open('articles.txt', 'w') as article:
+                    json.dump(n_articles, article, indent = 4)
+                response = Response(status = 200, mimetype = 'application/json')
                 return response
-            with open('articles.txt', 'w') as article:
-                json.dump(n_articles, article, indent = 4)
-            response = Response(status = 200, mimetype = 'application/json')
-            return response
+            else:
+                # Unauthorized access
+                response = Response(status = 401, mimetype = 'application/json')
+                return response
         else:
-            response = Response(status = 401, mimetype = 'application/json')
+            # User not found
+            response = Response(status = 404, mimetype = 'application/json')
             return response
 
 
-api.add_resource(Articles, '/articles')
-api.add_resource(N_Articles, '/articles/<no_of_articles>')
+api.add_resource(Articles, '/articles') # Route 1
+api.add_resource(N_Articles, '/articles/<no_of_articles>') # Route 2
 
 # @app.route('/article/retrieve_metadata')
 # def retrieve_metadata():
--- users.py
+++ users.py
@@ -19,7 +19,7 @@ def authorization():
         else:
             return False
     else:
-        return False
+        return None
 
 
 class Users(Resource):
@@ -29,18 +29,22 @@ class Users(Resource):
         username = data['username']
         password = data['password']
         display_name = data['display_name']
+        # Check NULL condition of all fields
         if username == "" or password == "" or display_name == "":
             response = Response(status = 400, mimetype = 'application/json')
-            return jsonify(response)
+            return response
         email_validator = lepl.apps.rfc3696.Email()
         if not email_validator(username):
+            # Email validation
             response = Response(status = 400, mimetype = 'application/json')
-            return jsonify(response)
+            return response
         user_details = users_db.get_user_details(username)
         if user_details:
+            #User already exists
             response = Response(status = 409, mimetype = 'application/json')
             return response
         else:
+            # Create a new user
             users_db.create_user(username, password, display_name)
             response = Response(status = 201, mimetype = 'application/json')
             return response
@@ -50,42 +54,61 @@ class Users(Resource):
         username = data['username']
         password = data['password']
         new_password = data['new_password']
+        # Check NULL condition of all fields
         if username == "" or password == "" or new_password == "":
             response = Response(status = 400, mimetype = 'application/json')
-            return jsonify(response)
+            return response
         auth = authorization()
-        if auth:
-            authentication = request.authorization
-            if authentication.username == username and authentication.password == password:
-                users_db.update_password(username, new_password)
-                response = Response(status = 200, mimetype = 'application/json')
-                return response
+        # Check authentication
+        if auth is not None:
+            if auth:
+                authentication = request.authorization
+                if authentication.username == username and authentication.password == password:
+                    users_db.update_password(username, new_password)
+                    response = Response(status = 200, mimetype = 'application/json')
+                    return response
+                else:
+                    # Unauthorized access
+                    response = Response(status = 401, mimetype = 'application/json')
+                    return response
             else:
+                # Unauthorized access
                 response = Response(status = 401, mimetype = 'application/json')
                 return response
         else:
-            response = Response(status = 401, mimetype = 'application/json')
+            # User not found
+            response = Response(status = 404, mimetype = 'application/json')
             return response
 
     def delete(self):
         data = request.get_json()
         username = data['username']
         password = data['password']
+        # Check NULL condition of all fields
         if username == "" or password == "" or password == "":
             response = Response(status = 400, mimetype = 'application/json')
-            return jsonify(response)
+            return response
         auth = authorization()
-        if auth:
-            authentication = request.authorization
-            if authentication.username == data['username'] and authentication.password == data['password']:
-                users_db.delete_user(data)
-                response = Response(status = 200, mimetype = 'application/json')
-                return response
+        # Check authentication
+        if auth is not None:
+            if auth:
+                authentication = request.authorization
+                if authentication.username == data['username'] and authentication.password == data['password']:
+                    # Delete a user
+                    users_db.delete_user(data)
+                    response = Response(status = 200, mimetype = 'application/json')
+                    return response
+                else:
+                    # Unauthorized access
+                    response = Response(status = 401, mimetype = 'application/json')
+                    return response
             else:
+                # Unauthorized access
                 response = Response(status = 401, mimetype = 'application/json')
                 return response
         else:
-            response = Response(status = 401, mimetype = 'application/json')
+            # User not found
+            response = Response(status = 404, mimetype = 'application/json')
             return response
 
 
